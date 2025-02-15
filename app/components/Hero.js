03255d30d6f2f914bc318a9af14826762b4e7ca3
corgab <corbanigabriele@gmail.com>
2025-02-16 00:50:55 +0100

    add hero animation

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,23 +1,61 @@
+'use client';
 import { EnvelopeIcon } from '@heroicons/react/24/solid';
 import LinkedInIcon from './LinkedInIcon';
-import Image from 'next/image';
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Hero() {
+  useGSAP(() => {
+    gsap.to(['.socials', '.surname'], {
+      x: '-500',
+      scrollTrigger: {
+        trigger: '.socials',
+        start: 'top center',
+        end: 'top top',
+        scrub: 1,
+        ease: 'none',
+        // markers: true,
+      },
+    });
+
+    gsap.to('.name', {
+      x: '500',
+      scrollTrigger: {
+        trigger: '.socials',
+        start: 'top center',
+        end: 'top top',
+        scrub: 1,
+        ease: 'none',
+      },
+    });
+  }, []);
+
   return (
     <>
-      <div className="container mx-auto p-4 h-screen w-full">
-        <div className="flex justify-between items-center h-full ">
-          <div className="space-y-5">
-            <a href="#" className="rounded-full bg-secondary-200 p-2 block">
+      <div className="container mx-auto p-4 h-screen w-full mb-96">
+        <div className="flex justify-between items-center h-full mb-96">
+          <div className="space-y-5 socials ">
+            <a
+              href="mailto:corbanigabriele@gmail.com"
+              className="rounded-full bg-secondary-200 p-2 block"
+            >
               <EnvelopeIcon className="text-white w-5 h-5 md:w-10 md:h-10" />
             </a>
-            <a href="#" className="rounded-full bg-secondary-200 p-2 block">
+            <a
+              href="https://www.linkedin.com/in/gabriele-corbani-01a11a315/"
+              target="_blank"
+              className="rounded-full bg-secondary-200 p-2 block"
+            >
               <LinkedInIcon className="text-white w-5 h-5 md:w-10 md:h-10" />
             </a>
           </div>
-          <div className="text-6xl md:text-8xl lg:text-9xl font-josefin font-black md:space-y-4 lg:space-y-8 tracking-7 leading-none uppercase">
-            <h1 className="text-primary-200">GABRIELE</h1>
+          <div className="text-6xl md:text-8xl lg:text-9xl font-josefin font-black md:space-y-4 lg:space-y-8 tracking-7 leading-none uppercase ">
+            <h1 className="text-primary-200 text-start name">GABRIELE</h1>
             <h1
-              className="text-white uppercase text-end"
+              className="text-white uppercase text-end surname"
               style={{
                 textShadow:
                   '-1px 0 0 #000, 0 1px 0 #000, 1px 0 0 #000, 0 -1px 0 #000',
--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="it" suppressHydrationWarning>
-      <body suppressHydrationWarning className=" overflow-hidden">
+      <body suppressHydrationWarning className=" ">
         {children}
       </body>
     </html>
