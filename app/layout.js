ce9d830dbecefbb90a7157e21423ad53b4ff602e
corgab <corbanigabriele@gmail.com>
2025-02-16 00:52:03 +0100

    overflow

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="it" suppressHydrationWarning>
-      <body suppressHydrationWarning className=" ">
+      <body suppressHydrationWarning className="overflow-hidden">
         {children}
       </body>
     </html>
