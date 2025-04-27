034b9319c7222c4a0db20b6a20f3eec71384c76d
corgab <corbanigabriele@gmail.com>
2025-04-28 00:34:18 +0200

    ADD img config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['dashboard.craftedhub.it'],
+  },
+};
 
 export default nextConfig;
